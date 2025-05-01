3d5b8e31fa06ad7fb2f228dfa72442ebba766ba0
Zakaria12e <zakariael1212@gmail.com>
2025-05-01 19:14:08 +0100

    refactor: remove MyApp component and associated imports from _app.tsx

--- src/app/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '@/lib/i18n';
-import { AppProps } from 'next/app';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
